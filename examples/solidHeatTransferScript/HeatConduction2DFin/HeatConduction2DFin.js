a16d29d78240543c8834c99e0cef809a18083a8b
nikoscham <nikoscham@gmail.com>
2025-06-02 09:54:20 +0300

    Remove HTML examples and add Node.js implementations for heat conduction simulations

---
+++ examples/solidHeatTransferScript/HeatConduction2DFin/HeatConduction2DFin.js
@@ -0,0 +1,51 @@
+//   ______ ______           _____           _       _     //
+//  |  ____|  ____|   /\    / ____|         (_)     | |    //
+//  | |__  | |__     /  \  | (___   ___ ____ _ ____ | |_   //
+//  |  __| |  __|   / /\ \  \___ \ / __|  __| |  _ \| __|  //
+//  | |    | |____ / ____ \ ____) | (__| |  | | |_) | |    //
+//  |_|    |______/_/    \_\_____/ \___|_|  |_|  __/| |    //
+//                                            | |   | |    //
+//                                            |_|   | |_   //
+//       Website: https://feascript.com/             \__|  //
+
+// Import Math.js
+import * as math from "mathjs";
+global.math = math;
+
+// Import FEAScript library
+import { FEAScriptModel, logSystem, VERSION } from "feascript";
+
+console.log('FEAScript Version:', VERSION);
+
+// Create a new FEAScript model
+const model = new FEAScriptModel();
+
+// Set solver configuration
+model.setSolverConfig("solidHeatTransferScript");
+
+// Define mesh configuration
+model.setMeshConfig({
+  meshDimension: "2D",
+  elementOrder: "quadratic",
+  numElementsX: 8,
+  numElementsY: 4,
+  maxX: 4,
+  maxY: 2,
+});
+
+// Define boundary conditions
+model.addBoundaryCondition("0", ["constantTemp", 200]);
+model.addBoundaryCondition("1", ["symmetry"]);
+model.addBoundaryCondition("2", ["convection", 1, 20]);
+model.addBoundaryCondition("3", ["constantTemp", 200]);
+
+// Set solver method (optional)
+model.setSolverMethod("lusolve");
+
+// Solve the problem and get the solution
+const { solutionVector, nodesCoordinates } = model.solve();
+
+// Print results to console
+console.log("Solution vector:", solutionVector);
+console.log("Node coordinates:", nodesCoordinates);
+console.log(`Number of nodes in mesh: ${nodesCoordinates.nodesXCoordinates.length}`);
