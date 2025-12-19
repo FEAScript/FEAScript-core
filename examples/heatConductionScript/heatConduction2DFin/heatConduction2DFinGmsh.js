53c013a759981f98d9241c4ca1b3b8d065b0bc96
nikoscham <nikoscham@gmail.com>
2025-12-19 11:27:45 +0200

    Rename rect_quad_unstruct.msh to rect_quad.msh in the node.js examples

--- examples/heatConductionScript/heatConduction2DFin/heatConduction2DFinGmsh.js
+++ examples/heatConductionScript/heatConduction2DFin/heatConduction2DFinGmsh.js
@@ -25,7 +25,7 @@ console.log("FEAScript Version:", printVersion);
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 // Read the mesh file
-const meshFilePath = path.join(__dirname, "rect_quad_unstruct.msh");
+const meshFilePath = path.join(__dirname, "rect_quad.msh");
 const meshContent = fs.readFileSync(meshFilePath, "utf8");
 
 async function main() {
@@ -38,7 +38,7 @@ async function main() {
   // Create a mock File object for Node.js environment
   const mockFile = {
     text: async () => meshContent,
-    name: "rect_quad_unstruct.msh",
+    name: "rect_quad.msh",
   };
 
   // Parse the mesh data
